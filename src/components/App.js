730385c556932697bcee4ed46d37a5d04925545c
mosam <mosamtijare1@gmail.com>
2023-02-05 22:37:40 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React, {useEffect, useState} from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-//code here 
+  //code here
+  const [value, setValue] = useState(true);
+
+  const [className, setClassName] = useState("redColor");
+
+  const onButtonClick = () => {
+    setValue(!value);
+  };
+
+  useEffect(() => {
+    if (value) {
+      setClassName("redColor");
+    }
+
+    if (!value) {
+      setClassName("blueColor");
+    }
+  }, [value]);
+
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={className}>Newton School</p>
+      <button id="button" onClick={onButtonClick}>
+        Change Style
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
